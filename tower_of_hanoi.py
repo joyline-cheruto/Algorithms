11f8ce2d4adb232ba9ac5ea917883d09feab0dc6
joyline-cheruto <joycheruto832@gmail.com>
2024-07-01 17:39:55 +0300

    output the moves as per no. of disks.

--- tower_of_hanoi.py
+++ tower_of_hanoi.py
@@ -1,8 +1,12 @@
 def tower_of_hanoi(n, A, B, C):
     if n == 1:
-        print(" move disk from ",A,"to",C)
+        print(" move disk from ", A, "to", C)
         return 1
     else:
-        tower_of_hanoi(n-1,A,C,B)
-        move (A,C)
-        tower_of_hanoi(n-1,B,A,C)
+        tower_of_hanoi(n - 1, A, C, B)
+        print("Move disk from", A, "to", C)
+        tower_of_hanoi(n - 1, B, A, C)
+
+
+n = int(input("Enter Number of disks:"))
+tower_of_hanoi(n, "A", "B", "C")
