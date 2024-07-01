32bb2e9be2f78fcd8977615a7f340bf6012b78c8
joyline-cheruto <joycheruto832@gmail.com>
2024-07-01 17:22:19 +0300

    order of disk movement

--- tower_of_hanoi.py
+++ tower_of_hanoi.py
@@ -2,3 +2,7 @@ def tower_of_hanoi(n, A, B, C):
     if n == 1:
         print(" move disk from ",A,"to",C)
         return 1
+    else:
+        tower_of_hanoi(n-1,A,C,B)
+        move (A,C)
+        tower_of_hanoi(n-1,B,A,C)
